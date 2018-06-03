b249112ad49f00ca2cb0ef1a2a3f82ff7e07163a
srinivas <compmanic@yahoo.com>
2018-06-02 22:18:11 -0700

    Three sum without hashing.

--- MyLeetcode/src/ThreeSum.java
+++ MyLeetcode/src/ThreeSum.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,7 +16,7 @@ public class ThreeSum {
     boolean found = false;
     Set<Integer> set = new HashSet<Integer>();
     for (int i = 0; i < a.length - 2; i++) {// need at least 3 elements to find a triplet.
-      set.clear(); // clear after i^th iteration.
+      set.clear(); // clear all j values after each i^th iteration.
       for (int j = i + 1; j < a.length; j++) {
         int sum = -(a[i] + a[j]);
         if (set.contains(sum)) {
@@ -31,9 +32,37 @@ public class ThreeSum {
       System.out.println("No solution found.");
   }
 
+  static void threeSum1(int[] a) {
+    Arrays.sort(a);
+    for (int i = 0; i < a.length; i++) {
+      if (i != 0 && a[i] == a[i - 1]) {
+        continue;
+      }
+      int j = i + 1, k = a.length - 1;
+      while (j < k) {
+        int sum = a[i] + a[j] + a[k];
+        if (sum == 0) {
+          System.out.println("Three sum found: " + a[i] + " " + a[j] + " " + a[k]);
+          j++;
+          k--;
+          while (j < k && a[j - 1] == a[j])
+            j++;
+          while (j < k && a[k + 1] == a[k])
+            k--;
+        } else if (sum > 0) {
+          k--;
+        } else {
+          j++;
+        }
+      }
+    }
+  }
+
   public static void main(String[] args) {
     int[] test = {-1, 0, 1, 2, -1, -4};
     threeSum(test);
+    System.out.println();
+    threeSum1(test);
   }
 
 }
